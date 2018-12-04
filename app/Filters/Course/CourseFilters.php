9aad9b70557109f4d5ba750ed79eca4d830281ec
MadalinGavrila <madalin.gavrila13@yahoo.com>
2018-12-04 18:19:42 +0200

    Laravel Filtering

---
+++ app/Filters/Course/CourseFilters.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Filters\Course;
+
+use App\Filters\Course\Ordering\ViewsOrder;
+use App\Filters\FiltersAbstract;
+
+class CourseFilters extends FiltersAbstract
+{
+    protected $filters = [
+        'access' => AccessFilter::class,
+        'difficulty' => DifficultyFilter::class,
+        'type' => TypeFilter::class,
+        'subject' => SubjectFilter::class,
+        'started' => StartedFilter::class,
+        'views' => ViewsOrder::class,
+    ];
+}
